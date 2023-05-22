215a77449018e7a8e289ac13453d86822cfb37ca
ben.arce <ben.arce12kires@gmail.com>
2023-05-22 18:01:03 -0400

    POR-2
    Se agregan iocnos de la app y colores iniciales

---
+++ YouTubeClone1/YouTubeClone1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone1
+//
+//  Created by Benjamin Arce on 22-05-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
